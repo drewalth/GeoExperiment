884821bdb1e48e39fd7cc5b25cdac2f8cdf5f523
Drew <andrew.althage@gmail.com>
2022-11-08 14:55:07 -0700

    Initial Commit

---
+++ GeoExperiment/GeoExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoExperimentApp.swift
+//  GeoExperiment
+//
+//  Created by Andrew Althage on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GeoExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
